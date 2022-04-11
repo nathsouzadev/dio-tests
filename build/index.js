d60497001dd608954e64adbd240a8d0645d01ba3
Nathally Souza <nathallyccd@gmail.com>
2022-04-11 14:25:50 -0300

    update gitignore

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-app_1.app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server on port ${process.env.PORT} \nhttp://localhost:${process.env.PORT}`);
-});
